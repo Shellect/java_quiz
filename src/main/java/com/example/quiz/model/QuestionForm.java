aacb77497833519377f00644fd5c4b030c5f2064
Oleg E <evstyunino@gmail.com>
2025-04-24 14:12:02 +0300

    fix form mapping error

--- src/main/java/com/example/quiz/model/QuestionForm.java
+++ src/main/java/com/example/quiz/model/QuestionForm.java
@@ -1,13 +1,33 @@
 package com.example.quiz.model;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 public class QuestionForm {
-    private Map<Long, String> question = new HashMap<>();
+    private List<Long> questionId;
+    private List<String> answer;
 
-    // Геттеры и сеттеры
-    public Map<Long, String> getUserAnswers() {
-        return question;
+    public QuestionForm() {
+    }
+
+    public QuestionForm(List<Long> questionId, List<String> answer) {
+        this.questionId = questionId;
+        this.answer = answer;
+    }
+
+    public void setQuestionId(List<Long> questionId) {
+        this.questionId = questionId;
+    }
+
+    public void setAnswer(List<String> answer) {
+        this.answer = answer;
+    }
+
+    public HashMap<Long, String> getUserAnswers() {
+        HashMap<Long, String> userAnswers = new HashMap<>();
+        for (int i = 0; i < questionId.size(); i++) {
+            userAnswers.put(questionId.get(i), answer.get(i));
+        }
+        return userAnswers;
     }
 }
